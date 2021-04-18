91a1363b561aa407d609820b0b88a66e7e183e70
Mami <gultekinmsg@gmail.com>
2021-04-18 21:57:57 +0300

    Added Swagger documentation

--- src/main/java/com/appcent/todoapp/TodoappApplication.java
+++ src/main/java/com/appcent/todoapp/TodoappApplication.java
@@ -2,8 +2,10 @@ package com.appcent.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TodoappApplication {
 
     public static void main(String[] args) {
